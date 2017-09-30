8f784f18b91ee32f5687f211c5771dbb3b44ae3b
guestl <boris.nikulin@gmail.com>
2017-09-30 11:42:59 +0300

    Fixed Wallie comments

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Prettify JSON
 
-[TODO. There will be project description]
+This script loads file with json data and print pretty output
 
 # Quickstart
 
@@ -13,7 +13,28 @@ Example of script launch on Linux, Python 3.5:
 $ python pprint_json.py <path to file>
 # TODO add output example
 
-```
+{
+    "glossary": {
+        "title": "example glossary",
+        "GlossDiv": {
+            "title": "S",
+            "GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+                    "SortAs": "SGML",
+                    "GlossTerm": "Standard Generalized Markup Language",
+                    "Acronym": "SGML",
+                    "Abbrev": "ISO 8879:1986",
+                    "GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                        "GlossSeeAlso": ["GML", "XML"]
+                    },
+                    "GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
 
 # Project Goals
 
--- pprint_json.py
+++ pprint_json.py
@@ -1,19 +1,32 @@
 import json
 import codecs
+import sys
+import os.path
 
 
 def load_data(filepath):
     if filepath:
         with codecs.open(filepath, 'r', 'utf8') as f:
-            data = f.read()
-    return data
+            json_file_data = f.read()
+    return json_file_data
 
 
-def pretty_print_json(data):
-    parsed = json.loads(data)
-    print(json.dumps(parsed, indent=4, sort_keys=True, ensure_ascii=False))
+def pretty_print_json(json_data):
+    parsed_json_data = json.loads(json_data)
+    print(json.dumps(parsed_json_data,
+          indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    data = load_data("datafile")
-    pretty_print_json(data)
+    if len(sys.argv) != 2:
+        print("You have to use json file name as parameters. \
+               \nExample: python pprint_json.py <path to json file>")
+        exit()
+
+    json_filename = sys.argv[1]
+
+    if os.path.isfile(json_filename):
+        readed_data = load_data(json_filename)
+        pretty_print_json(readed_data)
+    else:
+        print("Json file '%s' must exists" % json_filename)
